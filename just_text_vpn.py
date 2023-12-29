5e5bac296ba4550922172ac65f5b15f98f7c7b36
RaysonNB <97947041+RaysonNB@users.noreply.github.com>
2023-12-29 23:51:12 +0800

    Create just_text_vpn.py

---
+++ just_text_vpn.py
@@ -0,0 +1,25 @@
+import google.generativeai as genai
+#GOOGLE_API_KEY=userdata.get('AIzaSyCnIphJLd9FdZq_cXiSItw-2yvENi8-03k')
+genai.configure(api_key='AIzaSyCnIphJLd9FdZq_cXiSItw-2yvENi8-03k')
+model = genai.GenerativeModel('gemini-pro')
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
+response = model.generate_content("hello")
+
+print(response.text)
+'''
+response = chat.send_message("Okay, how about a more detailed explanation to a high schooler?", stream=True)
+
+for chunk in response:
+  print(chunk.text)
+  print("_"*80)
+'''
+import PIL.Image
+
+img = PIL.Image.open('image.jpg')
+model = genai.GenerativeModel('gemini-pro-vision')
+response = model.generate_content(img)
+
+print(response.text)
